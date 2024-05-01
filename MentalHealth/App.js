5211aeb0eafda673c8650683ed159018ebf6817c
Prachi Bhatt <bprachi443@gmail.com>
2024-05-01 10:13:09 +0530

    Update App.js and add new files

--- MentalHealth/App.js
+++ MentalHealth/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Page1 from './components/Page1';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Page1/>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: 'red',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
